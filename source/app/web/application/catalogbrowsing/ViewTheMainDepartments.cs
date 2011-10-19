bebfe6a3e86e6e2a3e148b90d6cd2bbe6b9cab4f
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-10-19 17:42:50 -0600

    Updated

--- source/app/web/application/catalogbrowsing/ViewTheMainDepartments.cs
+++ source/app/web/application/catalogbrowsing/ViewTheMainDepartments.cs
@@ -5,15 +5,16 @@ namespace app.web.application.catalogbrowsing
 {
     public class ViewTheMainDepartments : IEncapsulateUserFunctionality
     {
-        IFindDepartments department_finder;
+        IFindDepartments department_repository;
 
-        public ViewTheMainDepartments(IFindDepartments department_finder){
-            this.department_finder = department_finder;
+        public ViewTheMainDepartments(IFindDepartments department_repository)
+        {
+            this.department_repository = department_repository;
         }
 
         public void process(IContainRequestDetails request)
         {
-           department_finder.get_the_main_departments(); 
+            department_repository.get_the_main_departments();
         }
     }
 }
