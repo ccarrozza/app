f0293fce6c43169ed980a77a9805bb202ece01b7
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-10-19 11:19:46 -0600

    Updated

---
+++ source/app.specs/CalculatorSpecs.cs
@@ -0,0 +1,14 @@
+﻿using Machine.Specifications;
+
+namespace app.specs
+{
+    public class CalculatorSpecs
+    {
+        public class when_adding_two_numbers
+        {
+            It should_return_the_sum = () =>
+                Calculator.add(2, 3).ShouldEqual(5);
+
+        } 
+    }
+}
---
+++ source/app/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace app
+{
+    public class Calculator
+    {
+        public static int add(int first, int second)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
