a169e96500f15cd4198bf1a6fd2fdfe0fab6c4b7
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-10-19 10:28:15 -0600

    Updated

--- build/configuration/git.rb
+++ build/configuration/git.rb
@@ -1,8 +1,8 @@
 configs ={
   :git => {
     :user => '20111017remote',
-    :remotes => potentially_change("remotes",__FILE__),
-    :repo => 'app'
+    :remotes => %w[rbohnet squillman alleyoopgarnett ccarrozza],
+    :repo => 'app' 
   }
 }
 configatron.configure_from_hash(configs)
