4d00c9e4b1eb0595f394a7ef0e7b3e42ce52c2ae
git_name <git_email>
2011-10-19 13:37:06 -0400

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,13 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
     public class Calculator
     {
         public int add(int first, int second)
         {
+            if (second < 0)
+                throw new ArgumentException("Second value cannot be negative.");
             return first + second;
         }
     }
