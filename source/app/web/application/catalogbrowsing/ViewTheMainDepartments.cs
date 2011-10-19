670790cd7b68ebd59ce885293a769b167b11ab67
Ralph Bohnet <rbohnet@gmail.com>
2011-10-19 17:41:12 -0600

    Pushing new changes

--- source/app/web/application/catalogbrowsing/ViewTheMainDepartments.cs
+++ source/app/web/application/catalogbrowsing/ViewTheMainDepartments.cs
@@ -1,12 +1,19 @@
-﻿using app.web.infrastructure;
+﻿using app.tasks;
+using app.web.infrastructure;
 
 namespace app.web.application.catalogbrowsing
 {
     public class ViewTheMainDepartments : IEncapsulateUserFunctionality
     {
+        IFindDepartments department_finder;
+
+        public ViewTheMainDepartments(IFindDepartments department_finder){
+            this.department_finder = department_finder;
+        }
+
         public void process(IContainRequestDetails request)
         {
-            throw new System.NotImplementedException();
+           department_finder.get_the_main_departments(); 
         }
     }
 }
