19e053b4a95459b147ceb3aca73c310e731b0384
git_name <git_email>
2011-10-19 20:22:28 -0400

    Pushing new changes

--- source/app.specs/ViewTheDepartmentsInADepartmentSpecs.cs
+++ source/app.specs/ViewTheDepartmentsInADepartmentSpecs.cs
@@ -1,4 +1,6 @@
 ﻿ using Machine.Specifications;
+ using app.models;
+ using app.tasks;
  using app.web.infrastructure;
  using developwithpassion.specifications.rhinomocks;
  using developwithpassion.specifications.extensions;
@@ -17,9 +19,28 @@ namespace app.specs
    
         public class when_run : concern
         {
-        
-            It first_observation = () =>        
-                
+            private Establish c = () =>
+                                      {
+                                          request = fake.an<IContainRequestDetails>();
+                                          department_repository = depends.on<IFindDepartments>();
+                                          report_engine = depends.on<IDisplayInformation>();
+                                          the_department = new DepartmentItem();
+
+                                          department_repository.setup(
+                                              x => x.get_a_single_department(the_department.name)).Return(the_department);
+                                      };
+
+            Because b = () =>
+                sut.process(request);
+
+            It should_tell_the_report_engine_to_display_the_departments_within_a_department = () =>
+                report_engine.received(x => x.display(details_of_a_single_department));
+
+            static IDisplayInformation report_engine;
+            static DepartmentItem details_of_a_single_department;
+            static IContainRequestDetails request;
+            static IFindDepartments department_repository;
+            private static DepartmentItem the_department;
         }
     }
 }
